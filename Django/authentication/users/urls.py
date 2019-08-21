00223e653fa82ddcc94fd61523aa92ad80881fac
leonardo185 <leonardo18598@gmail.com>
2019-08-21 12:14:22 +0530

    completed authentication

--- Django/authentication/users/urls.py
+++ Django/authentication/users/urls.py
@@ -1,1 +1,9 @@
 from django import path
+
+form . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view)
+]
--- Django/authentication/users/views.py
+++ Django/authentication/users/views.py
@@ -12,3 +12,17 @@ def index(request):
         "user": request.user
     }
     return render(request, "users/user.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "user/login.html", {"message": "Invalid credintials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message":"Loggoed out"})
